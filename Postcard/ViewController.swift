2619a1e8517579af217a606833c2037d096a1a65
dantiyo <Acton@Mac-Pro-Mr-R-2.local>
2014-10-20 16:11:32 +0200

    added enter name label now  working

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
     
     @IBOutlet weak var mailButton: UIButton!
     
@@ -33,8 +34,15 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        
+        enterNameTextField.text = " "
+        enterNameTextField.resignFirstResponder()
         
         mailButton.setTitle("mail sent", forState: UIControlState.Normal)
     }
